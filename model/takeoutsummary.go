8a77c642f4c678be85c70bfa03cc3511103ff1e0
trevor <tjswim@gmail.com>
2023-06-10 23:08:25 +0000

    more cleanup of model

--- model/takeoutsummary.go
+++ model/takeoutsummary.go
@@ -19,31 +19,7 @@ type TakeOutSummary struct {
 	SystemCapabilities       SystemCapabilities `json:"SystemCapabilities"`
 	Cycles [] 				 Cycle `json:"cycles"`
 	CyclesIncomplete bool `json:"cyclesIncomplete"`
-	Events           []struct {
-		StartTs        time.Time `json:"startTs"`
-		TimezoneOffset int       `json:"timezoneOffset"`
-		Duration       string    `json:"duration"`
-		Continuation   bool      `json:"continuation"`
-		EventType      string    `json:"eventType"`
-		SetPoint       struct {
-			SetPointType    string `json:"setPointType"`
-			ScheduleType    string `json:"scheduleType"`
-			Preconditioning bool   `json:"preconditioning"`
-			Targets         struct {
-				HeatingTarget float64 `json:"heatingTarget"`
-				CoolingTarget float64 `json:"coolingTarget"`
-			} `json:"targets"`
-			TouchedBy             string    `json:"touchedBy"`
-			TouchedWhen           time.Time `json:"touchedWhen"`
-			TouchedTimezoneOffset int       `json:"touchedTimezoneOffset"`
-			TouchedWhere          string    `json:"touchedWhere"`
-			TouchedUserID         string    `json:"touchedUserId"`
-			ScheduledStart        int       `json:"scheduledStart"`
-			ScheduledDay          int       `json:"scheduledDay"`
-			PreviousType          string    `json:"previousType"`
-			EmergencyHeatActive   bool      `json:"emergencyHeatActive"`
-		} `json:"setPoint"`
-	} `json:"events"`
+	Events           [] Event `json:"events"`
 	EventsIncomplete bool  `json:"eventsIncomplete"`
 	Rates            []any `json:"rates"`
 }
--- parser.go
+++ parser.go
@@ -8,5 +8,6 @@ import (
 func main() {
 	var dataMap = make(map[string] model.TakeOutSummary);
 	dataMap["a"] = model.TakeOutSummary{};
-	fmt.Println(dataMap["a"].StartTs);
+	fmt.Println(dataMap["a"].Events);
+	fmt.Println(dataMap["a"].Cycles);
 }
