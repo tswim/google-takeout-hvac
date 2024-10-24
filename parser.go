23a6bb8a102063fb9e8b58914e9600dd6f8f4ec5
trevor <tjswim@gmail.com>
2024-10-24 05:02:27 +0000

    parse as csv

--- parser.go
+++ parser.go
@@ -4,15 +4,30 @@ import (
 	"google-takeout/hvacparser/takeout"
 	"google-takeout/hvacparser/visual"
 	"net/http"
+	"fmt"
 )
 
 func main() {
 	//Gather all the data
 	var stats = takeout.TraverseFilesystem()
 	visual.Initialize(stats)
+	fmt.Println("date,name,stage1heat,stage2heat,stage3heat,auxheat,stage1cool,stage2cool,stage3cool,humidifier")
+	for i := 0; i < len(stats); i++ {
+		for j :=0; j < len(stats[i].Runtimes); j++ {
+			fmt.Print(stats[i].Runtimes[j].Name,"-01,",stats[i].Name,",");
+			fmt.Print(stats[i].Runtimes[j].Stage1Heat,",")
+			fmt.Print(stats[i].Runtimes[j].Stage2Heat,",")
+			fmt.Print(stats[i].Runtimes[j].Stage3Heat,",")
+			fmt.Print(stats[i].Runtimes[j].AuxHeat,",")
+			fmt.Print(stats[i].Runtimes[j].Stage1Cool,",")
+			fmt.Print(stats[i].Runtimes[j].Stage2Cool,",")
+			fmt.Print(stats[i].Runtimes[j].Stage3Cool,",")
+			fmt.Print(stats[i].Runtimes[j].Humidifier,"\n")
+		}
+	}
 	//Start building the charts
-	http.HandleFunc("/", httpserver)
-	http.ListenAndServe(":8080", nil)
+	//http.HandleFunc("/", httpserver)
+	//http.ListenAndServe(":8080", nil)
 }
 
 func httpserver(w http.ResponseWriter, _ *http.Request) {
